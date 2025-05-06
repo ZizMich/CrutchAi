4787f7766b3ef376bd8e98a5b36be91f5f3459f5
Azizbek Gulomov <110441778+ZizMich@users.noreply.github.com>
2025-05-06 22:42:55 +0200

    Add template

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div>
+        <h1>Hallo mit React!</h1>
+        <button onClick={() => alert("Klick!")}>Klick mich</button>
+      </div>
+    );
+  }
+  
+  export default App;
